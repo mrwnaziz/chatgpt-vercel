3756eba2538f7f7c293e8483d213ae525e098512
mrwnaziz <84028290+mrwnaziz@users.noreply.github.com>
2023-04-24 13:51:07 +0400

    disable setting

--- src/components/Avatar/system.tsx
+++ src/components/Avatar/system.tsx
@@ -2,10 +2,9 @@ import { FC } from 'react';
 
 const SystemAvatar: FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => (
   <img
-    className="bg-gray-600 px-2 py-2"
     width={32}
     height={32}
-    style={{ background: 'black', marginTop: 0, padding: 2 }}
+    style={{ background: 'black', marginTop: 0, padding: 4, borderRadius: 4 }}
     {...props}
     src="/mbrcgi-icon.png"
   />
