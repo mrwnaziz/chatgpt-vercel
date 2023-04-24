b86e472eff37955f02530a546e97664439aff488
mrwnaziz <84028290+mrwnaziz@users.noreply.github.com>
2023-04-24 13:28:54 +0400

    bug fix

--- src/components/Avatar/system.tsx
+++ src/components/Avatar/system.tsx
@@ -5,7 +5,7 @@ const SystemAvatar: FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => (
     className="bg-gray-600 px-2 py-2"
     width={32}
     height={32}
-    style="background:black; margin-top:0px; padding:2px;"
+    style={{ background: 'black', marginTop: 0, padding: 2 }}
     {...props}
     src="/mbrcgi-icon.png"
   />
